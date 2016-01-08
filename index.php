2e850855fec74026a2080b7ea1e5d4409ee1efde
Dery <Dery@Dery-THINK>
2016-01-08 00:20:04 -0500

    Initial deploy zcfb

--- index.php
+++ index.php
@@ -19,61 +19,33 @@ and open the template in the editor.
         $app_secret = '257bf39896d0a4dd33f6c53fd6dafdec';
         $redirect_url = 'http://zcfb.herokuapp.com/index.php';
 
-        $fb = new Facebook\Facebook([
-            'app_id' => $app_id,
-            'app_secret' => $app_secret,
-            'default_graph_version' => 'v2.2',
-        ]);
+        // init app with app id (APPID) and secret (SECRET)
+        FacebookSession::setDefaultApplication($app_id, $app_secret);
 
-        // Get the access token first
-        if (!isset($_SESSION['facebook_access_token'])) {
-            $helper = $fb->getRedirectLoginHelper();
-            try {
-                $accessToken = $helper->getAccessToken();
-            } catch (Facebook\Exceptions\FacebookResponseException $e) {
-                // When Graph returns an error
-                echo 'Graph returned an error: ' . $e->getMessage();
-                exit;
-            } catch (Facebook\Exceptions\FacebookSDKException $e) {
-                // When validation fails or other local issues
-                echo 'Facebook SDK returned an error: ' . $e->getMessage();
-                exit;
-            }
-            if (!isset($accessToken)) {
-                // Need to login first
-                $permissions = ['email']; // optional
-                echo "abc";
-                $loginUrl = $helper->getLoginUrl('http://zcfb.herokuapp.com/index.php', $permissions);
-                echo "bcd";
+// login helper with redirect_uri
+        $helper = new FacebookRedirectLoginHelper($redirect_url);
 
-                echo '<a href="' . $loginUrl . '">Log in with Facebook!</a>';
-            } else {
-                // Logged in already!
-                $_SESSION['facebook_access_token'] = (string) $accessToken;
-
-                // Now you can redirect to another page and use the
-                // access token from $_SESSION['facebook_access_token']
-            }
+        try {
+            $session = $helper->getSessionFromRedirect();
+        } catch (FacebookRequestException $ex) {
+            // When Facebook returns an error
+        } catch (Exception $ex) {
+            // When validation fails or other local issues
         }
 
-        // use the access token to retrieve the facebook data
-        if (isset($_SESSION['facebook_access_token'])) {
-            $fb->setDefaultAccessToken($_SESSION['facebook_access_token']);
+// see if we have a session
+        if (isset($session)) {
+            // graph api request for user data
+            $request = new FacebookRequest($session, 'GET', '/me');
+            $response = $request->execute();
+            // get response
+            $graphObject = $response->getGraphObject();
 
-            try {
-                $response = $fb->get('/me');
-                $userNode = $response->getGraphUser();
-            } catch (Facebook\Exceptions\FacebookResponseException $e) {
-                // When Graph returns an error
-                echo 'Graph returned an error: ' . $e->getMessage();
-                exit;
-            } catch (Facebook\Exceptions\FacebookSDKException $e) {
-                // When validation fails or other local issues
-                echo 'Facebook SDK returned an error: ' . $e->getMessage();
-                exit;
-            }
-
-            echo 'Logged in as ' . $userNode->getName();
+            // print data
+            echo print_r($graphObject, 1);
+        } else {
+            // show login url
+            echo '<a href="' . $helper->getLoginUrl() . '">Login</a>';
         }
         ?>        
 
