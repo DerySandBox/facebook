006afa88f367cac1e78942e4144ac6ace00dde29
dery.yang@hotmail.com <dery.yang@hotmail.com>
2016-01-10 02:40:36 -0500

    better

--- index.php
+++ index.php
@@ -29,10 +29,7 @@ and open the template in the editor.
         ]);
 
         // Get the access token first
-        if (!empty($_GET('logout'))) {
-            $_SESSION['facebook_access_token'] = ''; 
-        }
-        if (empty($_SESSION['facebook_access_token'])) {
+        if (!isset($_SESSION['facebook_access_token'])) {
             $helper = $fb->getRedirectLoginHelper();
             try {
                 $accessToken = $helper->getAccessToken();
